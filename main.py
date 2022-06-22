510b00baa649887721d6f341eafd67b5fa7c0cf2
iduel <irixt123@outlook.com>
2022-06-22 12:25:04 +0800

    .

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from optimise import *
 from tkinter import *
-from tkinter import ttk
 
 class GUI(Frame):
     def __init__(self, app):
@@ -12,13 +11,17 @@ class GUI(Frame):
         self.vsb = Scrollbar(self, orient="vertical", command=self.text.yview)
         self.text.configure(yscrollcommand=self.vsb.set)
         
-        self.button1 = Button(self, text="Power Optimisation", command=self.powerOptimisation)
-        self.button2 = Button(self, text="Memory Optimisation", command=self.memoryOptimisation)
+        self.button1 = Button(self, text="Powerplan", command=self.addPowerplan)
+        self.button2 = Button(self, text="Power Optimisation", command=self.powerOptimisation)
+        self.button3 = Button(self, text="Memory Optimisation", command=self.memoryOptimisation)
+        self.button4 = Button(self, text="Clear Logs", command=self.clearText)
         
         self.vsb.pack(side="right", fill="y")
         self.text.pack(side="right", fill="both", expand=True)
         self.button1.pack()
         self.button2.pack()
+        self.button3.pack()
+        self.button4.pack()
         
     def deco(func):
         def inner(self):
@@ -27,7 +30,15 @@ class GUI(Frame):
             self.text.see("end")
             self.text.configure(state='disabled')
         return inner
+    
+    @deco
+    def clearText(self):
+        self.text.delete('1.0', END)
         
+    @deco
+    def addPowerplan(self):
+        return self.text.insert("end", self.op.powerplan())
+    
     @deco
     def powerOptimisation(self):
         results = self.op.power_optimisation()
